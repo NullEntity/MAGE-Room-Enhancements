f47cb9bceeecd10d41ced94e1fc78416919e5e11
Bryce Walther <bryce@brycewalther.com>
2012-10-12 02:07:37 -0500

    Added a debug message

--- RoomEnhancements.user.js
+++ RoomEnhancements.user.js
@@ -2,7 +2,7 @@
 // @name           plug.dj MAGE Room Enhancements
 // @namespace      Dis
 // @include        /^https?://(www\.)?plug\.dj/mage/$/
-// @version        1
+// @version        1.0.1
 // @updateURL      https://raw.github.com/NullEntity/MAGE-Room-Enhancements/master/RoomEnhancements.user.js
 // @downloadURL    https://raw.github.com/NullEntity/MAGE-Room-Enhancements/master/RoomEnhancements.user.js
 // ==/UserScript==
@@ -625,6 +625,7 @@ if (isBoris())
 //github
 
 javascript: (function () { 
+alert("loading");
 var jsCode = document.createElement('script'); 
 jsCode.setAttribute('id', 'plugbot-js'); 
 jsCode.setAttribute('src', 'https://raw.github.com/NullEntity/MAGE-Room-Enhancements/master/RoomEnhancements.user.js'); 
